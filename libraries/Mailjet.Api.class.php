98102dc1c6af0d2218b1b33dcd2d092787b632e5
FerhanGit <fismailov@mailjet.com>
2015-04-30 11:28:13 +0300

    * added user agent (prestashop-3.0) tracking

--- libraries/Mailjet.Api.class.php
+++ libraries/Mailjet.Api.class.php
@@ -254,7 +254,7 @@ class Mailjet_Api
     	curl_setopt($this->_curl_handle, CURLOPT_HTTPHEADER, array("Content-Type: ".$contType));
     	curl_setopt($this->_curl_handle, CURLOPT_SSL_VERIFYPEER, FALSE);
     	curl_setopt($this->_curl_handle, CURLOPT_SSL_VERIFYHOST, 2);
-    	
+    	curl_setopt($this->_curl_handle, CURLOPT_USERAGENT, 'prestashop-3.0');
     	curl_setopt($this->_curl_handle, CURLOPT_USERPWD, $this->_apiKey.':'.$this->_secretKey);
     	
     	if ($lastID) {
@@ -703,7 +703,8 @@ class Mailjet_Api
         curl_setopt($this->_curl_handle, CURLOPT_SSL_VERIFYHOST, 2);
         curl_setopt($this->_curl_handle, CURLOPT_TIMEOUT, 10); //timeout in seconds
         curl_setopt($this->_curl_handle, CURLOPT_USERPWD, $this->_apiKey.':'.$this->_secretKey);
-
+        curl_setopt($this->_curl_handle, CURLOPT_USERAGENT, 'prestashop-3.0');
+    	
         switch ($request) {
             case 'GET' :
                 curl_setopt($this->_curl_handle, CURLOPT_CUSTOMREQUEST, 'GET');
