f9fc1af3395007f69a113b96a7fc95be25240506
FerhanGit <fismailov@mailjet.com>
2016-05-30 11:14:34 +0300

    Updated module key value

--- mailjet.php
+++ mailjet.php
@@ -125,7 +125,7 @@ class Mailjet extends Module
 		$this->description = $this->l('Create contact lists and client segment groups, drag-n-drop newsletters, define client re-engagement triggers, follow and analyze all email user interaction, minimize negative user engagement events (blocked, unsubs and spam) and optimise deliverability and revenue generation. Get started today with 6000 free emails per month.');
 		$this->author = 'PrestaShop';
 		$this->version = '3.3.0';
-		$this->module_key = 'a5a406893178d66f7b17a4689623bd4b';
+		$this->module_key = 'c81a68225f14a65239de29ee6b78d87b';
 		$this->tab = 'advertising_marketing';
 
 		// Parent constructor
